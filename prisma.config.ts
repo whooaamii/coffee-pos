f3aabc5554a53013d69f282c3f5161e9f0c25a77
Revangga <rchivas107@gmail.com>
2025-12-26 10:25:17 +0800

    fix: correct user-session relation schema

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,11 +4,12 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: "./prisma/schema.prisma",
   migrations: {
-    path: "prisma/migrations",
+    path: "./prisma/migrations",
   },
   datasource: {
     url: process.env["DATABASE_URL"],
   },
 });
+
