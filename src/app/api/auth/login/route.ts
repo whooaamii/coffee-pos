7d3a9bad3c73c53a8a9c95d8eee40dc8ecc35fc6
Revangga <rchivas107@gmail.com>
2025-12-26 12:25:04 +0800

    fix: await cookies() in auth login route

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,63 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import { verifyPassword } from "@/lib/password";
+import { cookies } from "next/headers";
+
+export async function POST(req: Request) {
+    try {
+        const { email, password } = await req.json();
+
+        if (!email || !password) {
+            return NextResponse.json(
+                { error: "Email dan password wajib diisi" },
+                { status: 400 }
+            );
+        }
+
+        const user = await prisma.user.findUnique({
+            where: { email },
+        });
+
+        if (!user) {
+            return NextResponse.json(
+                { error: "User tidak ditemukan" },
+                { status: 404 }
+            );
+        }
+
+        const valid = await verifyPassword(password, user.password);
+        if (!valid) {
+            return NextResponse.json(
+                { error: "Password salah" },
+                { status: 401 }
+            );
+        }
+
+        const session = await prisma.session.create({
+            data: {
+                userId: user.id,
+                expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24), // 1 hari
+            },
+        });
+
+        const cookieStore = await cookies();
+
+        cookieStore.set("session", session.id, {
+            httpOnly: true,
+            path: "/",
+            sameSite: "lax",
+
+
+        });
+
+        return NextResponse.json({
+            role: user.role,
+        });
+    } catch (err) {
+        console.error(err);
+        return NextResponse.json(
+            { error: "Internal server error" },
+            { status: 500 }
+        );
+    }
+}
